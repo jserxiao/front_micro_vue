2b673c6ee6c63fc3c3184b1220bf1278f1bdf97f
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-21 23:33:30 +0800

    feat: 修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,11 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
+  rules: {
+    "semi": ["error", "always"]
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -11,5 +14,5 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
-}
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/subvue',
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  base: "/subvue",
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
